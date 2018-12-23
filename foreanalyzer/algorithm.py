8d66550a894304c0f62f985c1410f56ed1561b3a
Federico Lolli <federico123579@gmail.com>
2018-12-24 00:42:07 +0100

    Add timeframe normalizer

--- foreanalyzer/algorithm.py
+++ foreanalyzer/algorithm.py
@@ -7,9 +7,11 @@ Base algorithm module.
 
 import abc
 
-import pandas
+import numpy as np
 
-from foreanalyzer._internal_utils import (ACC_TIMEFRAMES, ACC_CURRENCIES)
+from foreanalyzer.data_handler import DataHandler
+from foreanalyzer._internal_utils import (
+    ACC_TIMEFRAMES, ACC_CURRENCIES, norm_timeframe)
 from foreanalyzer.exceptions import PeriodNotExpected
 
 # logger
@@ -17,12 +19,12 @@ import logging
 LOGGER = logging.getLogger("foreanalyzer.algo")
 
 
-class AbstractAlgorithm(abc.ABCMeta):
+class AbstractAlgorithm(metaclass=abc.ABCMeta):
     """abstract algo"""
 
-    def __init__(self, timeframe, acc_instrums, period):
+    def __init__(self, timeframe, acc_instrums, period, range_of_values):
         # timeframe of algo (1h, 1d, 10m, ...)
-        if timeframe not in ACCEPTED_TIMEFRAMES:
+        if timeframe not in ACC_TIMEFRAMES:
             raise ValueError("timeframe not accepted")
         self.timeframe = timeframe
         # accepted instuments
@@ -31,8 +33,11 @@ class AbstractAlgorithm(abc.ABCMeta):
                 raise ValueError("insturment {} not accepted".format(instr))
         self.accepted_instruments = acc_instrums
         # time interval of data feed
-        if not isinstance(period, int)
+        if not isinstance(period, int):
+            raise ValueError("period not an int")
         self.period = period
+        self.DH = DataHandler(range_of_values)
+        self.data = self.DH.data
 
     def check_period(self, period_of_values):
         if len(period_of_values) != self.period:
@@ -40,21 +45,95 @@ class AbstractAlgorithm(abc.ABCMeta):
         else:
             self.check_method(period_of_values)
 
-    @abc.abstractclassmethod
-    def check_method(self):
+    @abc.abstractmethod
+    def feed(self):
         pass
 
+    @abc.abstractmethod
+    def analyse(self):
+        pass
+
+
+class AlgorithmExample001(AbstractAlgorithm):
+    def __init__(self):
+        timeframe = ACC_TIMEFRAMES.ONE_HOUR
+        acc_instrums = [ACC_CURRENCIES.EURUSD]
+        period = 15
+        range_of_values = 50000
+        super().__init__(timeframe, acc_instrums, period, range_of_values)
+        # tools
+        self.sma = SMA(self.period)
+
+    def feed(self):
+        for instr in self.accepted_instruments:
+            # load data from csv
+            LOGGER.debug("loading data...")
+            self.DH.load_data(instr)
+            data = self.data[instr.value]
+            # fix timeframe
+            LOGGER.debug("fixing timeframe...")
+            len_pre_removal = len(data)
+            fix_timeframe(data, self.timeframe)
+            entries_removed = len_pre_removal - len(data)
+            LOGGER.debug("{} entries removed from {} rows".format(
+                entries_removed, len_pre_removal))
+            # evaluate sma
+            LOGGER.debug("calcolating sma...")
+            data = self.sma.eval(data)
+            # drop NaN
+            len_pre_removal = len(data)
+            data.dropna(inplace=True)
+            nan_removed = len_pre_removal - len(data)
+            LOGGER.debug("NaN removed {} from {} rows".format(
+                nan_removed, len_pre_removal))
+            return data
+
+    def analyse(self):
+        for instr in ACC_CURRENCIES:
+            pass
 
 
 #~~~~~~~~~~~~#
 # ALGO TOOLS #
 #~~~~~~~~~~~~#
 
-class base_tool(abc.ABCMeta):
+def fix_timeframe(df, timeframe):
+    for i in range(len(df)):
+        if i+1 < len(df):
+            while calc_diff_tf(df, i) < norm_timeframe(timeframe):
+                df.drop(df.index[i+1], inplace=True)
+                if i+1 >= len(df):
+                    break
+
+
+def calc_diff_tf(df, index):
+    return (df.loc[df.index[index+1], 'timestamp'] - df.loc[df.index[index], 'timestamp']).total_seconds()
+
+
+class base_tool(metaclass=abc.ABCMeta):
     def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def eval(self):
         pass
 
 
 class SMA(base_tool):
-    def __init__(self, ):
-        self.
+    def __init__(self, period):
+        self.period = period
+
+    def eval(self, df):
+        df['sma'] = np.nan  # set NaN
+        for i in range(len(df)):
+            i += 1  # scale up
+            # calc other index margin
+            i_p = i - self.period
+            # if n (period) of values avaible
+            if i_p >= 0:
+                close_index = df.columns.get_loc('close')
+                # calc simple moving average sum(n1+n2+n3) / nn
+                sma = sum(df.iloc[i_p:i, close_index]) / self.period
+                # set sma
+                df.at[df.index[i - 1], 'sma'] = sma
+        return df
--- foreanalyzer/tests/test_algorithm.py
+++ foreanalyzer/tests/test_algorithm.py
@@ -5,11 +5,72 @@ test.test_algorithm
 Test algos.
 """
 
-# import time
+from io import StringIO
+
+import numpy as np
+import pandas as pd
+
+from foreanalyzer.algorithm import SMA, AlgorithmExample001
+from foreanalyzer.data_handler import normalize_df
 
 # logger
 import logging
 LOGGER = logging.getLogger("foreanalyzer.tests.test_algorithm")
 LOGGER.info("TESTING test_algorithm.py module")
 
-def test_
+
+csv_example = StringIO('''<TICKER>,<DTYYYYMMDD>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>
+                          EURUSD,20010102,230100,0.9507,0.9507,0.9507,0.9507,4
+                          EURUSD,20010102,230200,0.9506,0.9506,0.9505,0.9505,4
+                          EURUSD,20010102,230300,0.9505,0.9507,0.9505,0.9506,4
+                          EURUSD,20010102,230400,0.9506,0.9506,0.9506,0.9506,4
+                          EURUSD,20010102,230500,0.9506,0.9506,0.9506,0.9506,4
+                          EURUSD,20010102,230600,0.9506,0.9506,0.9506,0.9506,4
+                          EURUSD,20010102,230700,0.9505,0.9507,0.9505,0.9507,4
+                          EURUSD,20010102,230800,0.9507,0.9507,0.9507,0.9507,4
+                          EURUSD,20010102,230900,0.9507,0.9507,0.9507,0.9507,4
+                          EURUSD,20010103,005000,0.9495,0.9496,0.9495,0.9496,4
+                          EURUSD,20010103,005100,0.9496,0.9496,0.9496,0.9496,4
+                          EURUSD,20010103,005200,0.9496,0.9496,0.9496,0.9496,4
+                          EURUSD,20010103,005300,0.9495,0.9495,0.9495,0.9495,4
+                          EURUSD,20010103,005400,0.9494,0.9494,0.9492,0.9492,4
+                          EURUSD,20010103,005500,0.9492,0.9493,0.9492,0.9493,4
+                          EURUSD,20010103,005600,0.9493,0.9497,0.9493,0.9497,4''')
+
+def test_SMA():
+    LOGGER.debug("RUN test_SMA")
+    df = normalize_df(pd.read_csv(csv_example), 1000)
+    # set period for testing
+    period = 4
+    sma = SMA(period)
+    close_values = df['close'].values
+    sma_values = sma.eval(df)['sma'].values
+    calc_sma = []
+    # add SMA
+    for i, val in enumerate(close_values):
+        i += 1  # scale up
+        # calc other index margin
+        i_p = i - period
+        if i < period:  # append NaN
+            calc_sma.append(np.nan)
+        # if n (period) of values avaible
+        elif i_p >= 0:
+            # calc simple moving average sum(n1+n2+n3) / nn
+            sma = sum(close_values[i_p:i]) / period
+            # append sma
+            calc_sma.append(sma)
+    # check every value
+    sma_values = pd.Series(sma_values).dropna()
+    calc_sma = pd.Series(calc_sma).dropna()
+    for i in range(len(sma_values)):
+        assert sma_values.iloc[i] == calc_sma.iloc[i]
+    LOGGER.debug(calc_sma.head())
+    LOGGER.debug("PASSED test_SMA")
+
+
+def test_feed():
+    LOGGER.debug("RUN test_feed")
+    algo = AlgorithmExample001()
+    data = algo.feed()
+    LOGGER.debug(data.tail())
+    LOGGER.debug("PASSED test_feed")

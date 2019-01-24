b42c96da31a280d6154a56b4e211257c8c92a52f
Federico Lolli <federico123579@gmail.com>
2019-01-24 23:41:11 +0100

    WIP

--- foreanalyzer/engine.py
+++ foreanalyzer/engine.py
@@ -5,8 +5,7 @@ foreanalyzer.engine
 Main engine for the simulation.
 """
 
-from foreanalyzer._internal_utils import (ACC_CURRENCIES, OUTER_FOLDER_PATH,
-                                          unzip_data)
+from foreanalyzer._internal_utils import read_config
 from foreanalyzer.data_handler import DataHandler
 from foreanalyzer.simulation import AccountSimulated
 
@@ -14,12 +13,13 @@ from foreanalyzer.simulation import AccountSimulated
 import logging
 LOGGER = logging.getLogger("foreanalyzer.engine")
 
-
+# Engine
 class EngineBridge(object):
     """engine"""
 
     def __init__(self):
+        config = read_config()
         # data to feed
-        self.handler = DataHandler()
+        self.handler = DataHandler(config['engine']['range_of_simulation'])
         # init simulation components
         self.account = AccountSimulated()
--- foreanalyzer/simulation.py
+++ foreanalyzer/simulation.py
@@ -7,6 +7,7 @@ Store the simulation objects.
 
 from foreanalyzer._internal_utils import ACC_CURRENCIES, read_config
 from foreanalyzer.account import Account
+from foreanalyzer.tables import CURRENCIES
 
 # logger
 import logging
@@ -22,11 +23,15 @@ class AccountSimulated(Account):
         initial_funds = self.config['simulation']['initial_funds']
         super().__init__(margin_mode, initial_funds)
 
-    def simulate(self, instrument, buy_ask_price, sell_bid_price):
+    def simulate_price(self, instrument, price):
         """update price_couple in account"""
         if instrument not in ACC_CURRENCIES:
             raise ValueError("Instrument not accepted")
         if instrument not in self.price_tables:
             self.price_tables[instrument] = {}
+        spread = CURRENCIES[instrument.value].spread
+        buy_ask_price = price + spread
+        sell_bid_price = price
         self.price_tables[instrument]['buy'] = buy_ask_price
         self.price_tables[instrument]['sell'] = sell_bid_price
+

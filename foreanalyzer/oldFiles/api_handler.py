70adf5a98e2dcba01aa808ae8ee352c5bcf228cf
Fede Ubuntu <federico123579@gmail.com>
2019-08-14 17:16:02 +0200

    New file organization -> alpha3

---
+++ foreanalyzer/oldFiles/api_handler.py
@@ -0,0 +1,48 @@
+"""
+foreanalyzer.api_handler
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+Store the simulated Account object.
+"""
+
+import logging
+
+import XTBApi.exceptions
+from XTBApi.api import Client
+
+import foreanalyzer._internal_utils as internal
+from foreanalyzer.exceptions import LoginFailed
+
+LOGGER = logging.getLogger("foreanalyzer.api_handler")
+
+
+class ApiClient(metaclass=internal.SingletonMeta):
+    """handler of Api, use only for singleton"""
+
+    def __init__(self):
+        self.api = Client()
+        self.status = internal.STATUS.OFF
+
+    def setup(self):
+        """login with credentials in config"""
+        if self.status == internal.STATUS.ON:
+            LOGGER.debug("ApiClient already setup")
+            return
+        config = internal.read_config()['account']
+        try:
+            response = self.api.login(config['username'], config['password'])
+        except XTBApi.exceptions.CommandFailed:
+            raise LoginFailed()
+        self.status = internal.STATUS.ON
+        LOGGER.debug(f"{self.__class__.__name__} setup")
+        return response
+
+    def shutdown(self):
+        """logout for symmetry"""
+        if self.status == internal.STATUS.OFF:
+            LOGGER.debug("ApiClient already shutdown")
+            return
+        response = self.api.logout()
+        self.status = internal.STATUS.OFF
+        LOGGER.debug(f"{self.__class__.__name__} shutdown")
+        return response
---
+++ foreanalyzer/oldFiles/exceptions.py
@@ -0,0 +1,73 @@
+"""
+foreanalyzer.exceptions
+~~~~~~~~~~~~~~~~~~~~~~~
+
+All exceptions here.
+"""
+
+import logging
+
+LOGGER = logging.getLogger("foreanalyzer.exceptions")
+
+
+class FundsExhausted(Exception):
+    def __init__(self):
+        self.msg = "Funds exhausted"
+        LOGGER.warning(self.msg)
+        super().__init__(self.msg)
+
+
+class LoginFailed(Exception):
+    def __init__(self):
+        self.msg = "Login failed"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class NotLogged(Exception):
+    def __init__(self):
+        self.msg = "Login needed not done yet"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class IndicatorNotExecuted(Exception):
+    def __init__(self):
+        self.msg = "Indicator not executed yet"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class IndicatorLenError(Exception):
+    def __init__(self, val, dat):
+        self.msg = f"Len of values {val} is different from dataframe len {dat}"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class CurrencyNotListed(Exception):
+    def __init__(self, currency):
+        self.msg = f"Currency {currency} not listed"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class IndicatorNotListed(Exception):
+    def __init__(self, indicator):
+        self.msg = f"Indicator {indicator} not listed"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class IndicatorError(Exception):
+    def __init__(self):
+        self.msg = "Indicator error (may be the filter)"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class AlgorithmNotExecuted(Exception):
+    def __init__(self):
+        self.msg = "Algorithm not executed yet"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)

99dc452a2e413a336f11b3bc365ed944e1e9756b
Fede Ubuntu <federico123579@gmail.com>
2019-08-18 00:21:33 +0200

    Test simulation updated to ema and sma

--- foreanalyzer/tests/test_simulation.py
+++ foreanalyzer/tests/test_simulation.py
@@ -21,40 +21,50 @@ LOGGER = logging.getLogger("foreanalyzer.tests.test_simulation")
 
 def test_SMA_Simulation():
     # set up conf for the algorithm
-    indicators = [{
-        'name': 'SMA',
-        'args': {'period': 12, 'timeframe': 3600}
-    }]
+    indicators = [
+        {'name': 'SMA',
+        'args': {'period': 10, 'timeframe': 86400}},
+        {'name': 'EMA',
+        'args': {'period': 20, 'timeframe': 86400}}]
     algo = BaseAlgorithm('TestAlgo', indicators)
     account = Account(1000)
     LOGGER.debug("components inited")
     algo.setup()
     account.setup()
     LOGGER.debug("components setup")
-    df = algo.dataframes[CURRENCY.EURUSD].dataframe
-    sma = algo.dataframes[CURRENCY.EURUSD].SMA
-    LOGGER.debug("got df and sma")
-    res_df = df.resample('3600S').asfreq()
-    res_sma = sma.values.resample('3600S').asfreq()
-    res_df['sma'] = res_sma
-    res_df.dropna(inplace=True)
-    LOGGER.debug(f"resampled & merged sma and dataframe to {len(res_df)}")
-    _open_entries = res_df[res_df['close'] + 0.003 < res_df['sma']].index
+    df_obj = algo.dataframes[CURRENCY.EURUSD]
+    _indicator_names_str = ", ".join([x for x in df_obj.indicator_names])
+    LOGGER.debug(f"got df_obj and indicators {_indicator_names_str}")
+    df_obj.merge_indicators()
+    res_df = df_obj.resample(3600).dropna()
+    LOGGER.debug(f"resampled & merged dataframe to {len(res_df)}")
+    # DEFINE OPEN ENTRIES
+    res_df.insert(0, 'count', range(len(res_df)))
+    _entries_1 = res_df[res_df['ema_20'] < res_df['sma_10']]
+    _open_entries_full = _entries_1[_entries_1['count'].diff() != 1]
+    _open_entries = _open_entries_full.index
     LOGGER.debug(f"got {len(_open_entries)} open entries")
     trade_parameters = []
+    # DEFINE CLOSE CONDITIONS
     for _entry in _open_entries:
         data_point = res_df.loc[_entry]
         new_parameter = [data_point['close']]
-        new_df = res_df[data_point.name:].copy()
-        new_parameter.append(new_df[new_df['close'] >
-                                    new_df['sma']].iloc[0]['close'])
-        new_parameter.append(data_point.name)
+        new_df = res_df[_entry:].copy()
+        close_entry = new_df[new_df['ema_20'] >
+                             new_df['sma_10']].iloc[0]
+        new_parameter.append(close_entry['close'])
+        new_parameter.append(_entry)
+        new_parameter.append(close_entry.name)
         trade_parameters.append(new_parameter)
     LOGGER.debug(f"got {len(trade_parameters)} trade parameters")
-    for open_pr, close_pr, close_date in trade_parameters:
+    for open_pr, close_pr, open_date, close_date in trade_parameters:
+        # DEFINE MODE AND VOLUME
         trade = account.open_trade(CURRENCY.EURUSD, MODE.buy, 0.01,
                                    open_pr)
-        trade.datetime = close_date
+        trade.open_price = open_pr
+        trade.close_price = close_pr
+        trade.open_datetime = open_date
+        trade.close_datetime = close_date
         account.close_trade(trade.trade_id, close_pr)
     LOGGER.debug("setup trades")
     trades_evaluated = account.evaluate_trades()
@@ -68,13 +78,17 @@ def test_SMA_Simulation():
     LOGGER.debug(f"Min profit at once: {min(profit_list)}")
     LOGGER.debug(f"Mean of profits: {sum(profit_list) / len(profit_list)}")
     # save a dataframe for later analysis
-    pickle_arguments = [[tr.datetime, tr.profit, tr.new_balance] for tr in
+    pickle_arguments = [[tr.open_datetime, tr.close_datetime, tr.open_price,
+                         tr.close_price, tr.profit, tr.new_balance] for tr in
                         trades_evaluated]
     pickle_df = pd.DataFrame({
-        'datetime': [x[0] for x in pickle_arguments],
-        'profit': [x[1] for x in pickle_arguments],
-        'balance': [x[2] for x in pickle_arguments]
-    }).set_index('datetime')
+        'open_datetime': [x[0] for x in pickle_arguments],
+        'close_datetime': [x[1] for x in pickle_arguments],
+        'open_price': [x[2] for x in pickle_arguments],
+        'close_price': [x[3] for x in pickle_arguments],
+        'profit': [x[4] for x in pickle_arguments],
+        'balance': [x[5] for x in pickle_arguments]
+    }).set_index('open_datetime')
     file_path = os.path.join(
         OUTER_FOLDER_PATH, "profit_dataframe.pickle")
     with open(file_path, 'wb') as f:

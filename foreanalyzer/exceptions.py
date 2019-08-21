23f3ac20ea1f0e70fa24c4a9d70b7967fda4765b
Fede Ubuntu <federico123579@gmail.com>
2019-08-21 20:49:47 +0200

    Added get_open_signals in base algorithm module and NewAlgo BollBands

--- foreanalyzer/exceptions.py
+++ foreanalyzer/exceptions.py
@@ -10,10 +10,52 @@ import logging
 LOGGER = logging.getLogger("foreanalyzer.exceptions")
 
 
+class AlgorithmNotExecuted(Exception):
+    def __init__(self):
+        self.msg = "Algorithm not executed yet"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class CurrencyNotListed(Exception):
+    def __init__(self, currency):
+        self.msg = f"Currency {currency} not listed"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
 class FundsExhausted(Exception):
     def __init__(self):
         self.msg = "Funds exhausted"
         LOGGER.warning(self.msg)
+        super().__init__(self.msg)
+
+
+class IndicatorError(Exception):
+    def __init__(self):
+        self.msg = "Indicator error (may be the filter)"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class IndicatorLenError(Exception):
+    def __init__(self, val, dat):
+        self.msg = f"Len of values {val} is different from dataframe len {dat}"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class IndicatorNotExecuted(Exception):
+    def __init__(self):
+        self.msg = "Indicator not executed yet"
+        LOGGER.error(self.msg)
+        super().__init__(self.msg)
+
+
+class IndicatorNotListed(Exception):
+    def __init__(self, indicator):
+        self.msg = f"Indicator {indicator} not listed"
+        LOGGER.error(self.msg)
         super().__init__(self.msg)
 
 
@@ -27,47 +69,5 @@ class LoginFailed(Exception):
 class NotLogged(Exception):
     def __init__(self):
         self.msg = "Login needed not done yet"
-        LOGGER.error(self.msg)
-        super().__init__(self.msg)
-
-
-class IndicatorNotExecuted(Exception):
-    def __init__(self):
-        self.msg = "Indicator not executed yet"
-        LOGGER.error(self.msg)
-        super().__init__(self.msg)
-
-
-class IndicatorLenError(Exception):
-    def __init__(self, val, dat):
-        self.msg = f"Len of values {val} is different from dataframe len {dat}"
-        LOGGER.error(self.msg)
-        super().__init__(self.msg)
-
-
-class CurrencyNotListed(Exception):
-    def __init__(self, currency):
-        self.msg = f"Currency {currency} not listed"
-        LOGGER.error(self.msg)
-        super().__init__(self.msg)
-
-
-class IndicatorNotListed(Exception):
-    def __init__(self, indicator):
-        self.msg = f"Indicator {indicator} not listed"
-        LOGGER.error(self.msg)
-        super().__init__(self.msg)
-
-
-class IndicatorError(Exception):
-    def __init__(self):
-        self.msg = "Indicator error (may be the filter)"
-        LOGGER.error(self.msg)
-        super().__init__(self.msg)
-
-
-class AlgorithmNotExecuted(Exception):
-    def __init__(self):
-        self.msg = "Algorithm not executed yet"
         LOGGER.error(self.msg)
         super().__init__(self.msg)

f011421536752f836b1e606d5a2676c713d0ded1
Fede Ubuntu <federico123579@gmail.com>
2019-08-23 01:29:44 +0200

    Fix sorting in opening and rework of close signals

--- foreanalyzer/algo_modules/boll_bands_1.py
+++ foreanalyzer/algo_modules/boll_bands_1.py
@@ -7,9 +7,7 @@ Based on Bolliger Bands with timeframe of an hour.
 
 import logging
 
-import pandas as pd
-
-from foreanalyzer._internal_utils import MODE, read_config
+from foreanalyzer._internal_utils import read_config
 from foreanalyzer.algorithm import BaseAlgorithm
 
 
@@ -41,21 +39,14 @@ class BolligerBands1(BaseAlgorithm):
         short_raw_signals = df[df['close'] > df['BollBands_20_up']]
         return short_raw_signals
 
-    def _close_signal_formula(self, df_obj, signals_df):
-        df = df_obj.resample(self.update_freq).dropna()
-        signals = []
-        for datetime, mode in signals_df.iteritems():
-            datetime_range = df.loc[datetime:]
-            if mode == MODE.buy.value:
-                catch = datetime_range[
-                    datetime_range['close'] > datetime_range['BollBands_20_ma']]
-            elif mode == MODE.sell.value:
-                catch = datetime_range[
-                    datetime_range['close'] < datetime_range['BollBands_20_ma']]
-            signal = catch.iloc[0] if not catch.empty else datetime_range[-1]
-            open_pr = datetime_range.iloc[0]['open']
-            close_pr = signal['close']
-            signals.append([datetime, signal.name, open_pr, close_pr, mode])
-        return pd.DataFrame(columns=['open_datetime', 'close_datetime',
-                                     'open_price', 'close_price', 'mode'],
-                            data=signals)
+    def _close_long_signal_formula(self, datetime_range, signals_df, df_obj):
+        catch = datetime_range[
+            datetime_range['close'] > datetime_range['BollBands_20_ma']]
+        long_signal = catch.iloc[0] if not catch.empty else datetime_range[-1]
+        return long_signal
+
+    def _close_short_signal_formula(self, datetime_range, signals_df, df_obj):
+        catch = datetime_range[
+            datetime_range['close'] < datetime_range['BollBands_20_ma']]
+        short_signal = catch.iloc[0] if not catch.empty else datetime_range[-1]
+        return short_signal

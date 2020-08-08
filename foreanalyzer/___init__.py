8827f3ba532e1f73838d896a92b3e60e9e738c2a
Federico Lolli <federico123579@gmail.com>
2020-08-08 15:41:23 +0200

    Fix logger

---
+++ foreanalyzer/___init__.py
@@ -0,0 +1,1 @@
+from foreanalyzer.__version__ import __version__
--- foreanalyzer/console.py
+++ foreanalyzer/console.py
@@ -4,6 +4,8 @@
 # ~~~~~~~~~~~~~~~~~~~~
 
 import logging
+import logging.config
+import os.path
 
 import rich
 
@@ -11,6 +13,43 @@ import foreanalyzer.utils as utils
 
 
 # ~ * LOGGER * ~
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'deafult': {
+            'format':
+                '%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+            'formatter': 'deafult',
+        },
+        'rotating': {
+            'class': 'logging.handlers.TimedRotatingFileHandler',
+            'formatter': 'deafult',
+            'filename': os.path.join(
+                os.path.dirname(__file__), 'logs', 'logfile.log'),
+            'when': 'midnight',
+            'backupCount': 3
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            #'propagate': True
+        },
+        'foreanalyzer': {
+            'handlers': ['rotating'],
+            'level': 'DEBUG'
+        }
+    }
+})
 LOGGER = logging.getLogger("foreanalyzer")
 
 
